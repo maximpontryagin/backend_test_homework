8b65e9756b6edca3c600f47a1730fb05161ae121
Максим Понтрягин <maximpontryagin@yandex.ru>
2023-09-14 00:23:09 +0300

    KYKY

---
+++ new.py
@@ -0,0 +1,12 @@
+new_list =list()
+for i in range (1, 11):
+    new_list.append(2**i)
+print(type(new_list))
+print(new_list)
+
+
+mas = []
+for i in range(1,11):
+    mas[i] = None
+
+print(mas[0])
